aa7db42b44a4baafe720249eaff009010578e380
Meryx <50329017+Meryx@users.noreply.github.com>
2020-02-15 05:03:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -86,6 +86,9 @@ bot.onText(/\/anime (.+)/, function(msg,match){
   var url = 'anime?filter[text]=cowboy%20bebop&page[limit]=1&page[offset]=0';
   var deets = kitsu.get(url);
   var data = deets.data[0];
+  var echo = data.canonicalTitle;
+
   bot.sendMessage(chatId, data.canonicalTitle);
 
 });
+bot.on("polling_error", (err) => console.log(err));
