39888d190f8d0848a0ee6e4033f8bdb30e98ce7a
Meryx <50329017+Meryx@users.noreply.github.com>
2020-02-15 04:51:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express');
 const path = require('path');
 const jsonapi = require('jsonapi-parse');
 const Kitsu = require('kitsu');
-const htmlToText = require('html-to-text');
-const bot = new TelegramBot(token, { polling: true });
-const kitsu = new Kitsu();
 const token = process.env.TOKEN || 5;
 const PORT = process.env.PORT || 5000
 const OMDBAPI = process.env.OMDBAPI || 5;
+const htmlToText = require('html-to-text');
+const bot = new TelegramBot(token, { polling: true });
+const kitsu = new Kitsu();
 
 
 express()
