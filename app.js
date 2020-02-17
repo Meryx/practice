c066251c94df1c726cd71df6916babda9be81240
Meryx <50329017+Meryx@users.noreply.github.com>
2020-02-17 23:15:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -102,9 +102,8 @@ bot.onText(/\/anime (.+)/, function(msg, match) {
     })
 });
 
-var hi =
 
-bot.onText(/^دبب$/,function (msg, match){
+bot.onText(/دبب/,function (msg, match){
   var chatId = msg.chat.id;
   bot.sendMessage(chatId, "دبب");
 });
